883a8c5b15d33ef8fa228725eb2b59d523a2c5d8
Rizal Fauzie Ridwan <rizal@fauzie.my.id>
2016-07-21 06:35:01 +0700

    test jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,37 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*!\n' +
+            ' * fauzie v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under the <%= pkg.license %> license\n' +
+            ' */\n',
+
+		concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: false
+      },
+      scripts: {
+        src: [
+          'js/transition.js',
+          'js/alert.js',
+          'js/button.js',
+          'js/carousel.js',
+          'js/collapse.js',
+          'js/dropdown.js',
+          'js/modal.js',
+          'js/tooltip.js',
+          'js/popover.js',
+          'js/scrollspy.js',
+          'js/tab.js',
+          'js/affix.js'
+        ],
+        dest: 'assets/js/<%= pkg.name %>.js'
+      }
+    },
+
 		jekyll: {
       options: {
         bundleExec: true,
@@ -25,8 +56,21 @@ module.exports = function (grunt) {
           raw: 'github: true'
         }
       }
-    }
+    },
 
+		uglify: {
+      options: {
+        compress: {
+          warnings: false
+        },
+        mangle: true,
+        preserveComments: /^!|@preserve|@license|@cc_on/i
+      },
+      main: {
+        src: '<%= concat.scripts.dest %>',
+        dest: 'dist/js/<%= pkg.name %>.min.js'
+      }
+    },
 
 
 	});
